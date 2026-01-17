452f888a2c35d7fc9149f0cdd6c74ac9bc784b0c
jldpDev <joseluisdp297@gmail.com>
2026-01-17 15:59:46 +0000

    app

---
+++ city.js
@@ -0,0 +1,84 @@
+// Referencebutton
+const calculate = document.querySelector('button')
+const messageContainer = document.getElementById("feedback")
+
+
+function displayImageCity(city){
+    const cityImage = document.querySelector('img')
+    switch(city){
+        case "tokyo":
+            cityImage.src = 'images/tokyo.jpg'
+            cityImage.alt = "Tokyo"
+            break
+        case "london":
+            cityImage.src = 'images/london.png'
+            cityImage.alt = "London"
+            break
+        case "paris":
+            cityImage.src = 'images/paris.png'
+            cityImage.alt = "Paris"
+            break
+        case "coruna":
+            cityImage.src = 'images/coruna.jpg'
+            cityImage.alt = "A Coruna"
+            break
+        case "newyork":
+            cityImage.src = 'images/newyork.jpg'
+            cityImage.alt = "New York"
+            break
+    }
+}
+
+function calculatePrice(city, nights, season){
+    let basecost = 50
+    let seasonExtra
+    let cityExtra
+    if(season == "1"){
+        seasonExtra = 1.5
+    }else if(season == "2"){
+        seasonExtra = 1.2
+    }
+    switch(city){
+        case "tokyo":
+            cityExtra = 1.5
+            break
+        case "london":
+            cityExtra = 1.4
+            break
+        case "paris":
+            cityExtra = 1.35
+            break
+        case "coruna":
+            cityExtra = 1.2
+            break
+        case "newyork":
+            cityExtra = 1.45
+            break
+    }
+
+    let priceNight = basecost*seasonExtra*cityExtra
+    let total = priceNight*nights
+    return total;
+}
+
+
+calculate.addEventListener('click', () => {
+    messageContainer.innerHTML = ''
+    
+    const selectCity = document.getElementById('city').value
+    const selectNights = document.getElementById('nights').value
+    const selectSeason = document.getElementById('season').value
+
+    displayImageCity(selectCity)
+    let totalCost = calculatePrice(selectCity, parseInt(selectNights), selectSeason)
+    
+    const message = document.createElement('p')
+    message.textContent = `Total cost for ${selectNights} nights in ${selectCity} is £${totalCost}`
+    message.style.fontSize = "2rem"
+    message.classList.add("text-center")
+    messageContainer.appendChild(message)
+
+
+})
+
+
